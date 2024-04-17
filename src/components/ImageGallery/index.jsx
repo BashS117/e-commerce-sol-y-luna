3a5bf52a122dff97f861d366692b480f830707a3
Sebastian Rodriguez <sebasrm2000@gmail.com>
2024-04-17 16:28:33 -0500

    fix style(product detail) 60ml image

--- src/components/ImageGallery/index.jsx
+++ src/components/ImageGallery/index.jsx
@@ -22,7 +22,7 @@ const ImageGallery = ({ img, img2, img3,selectedVolume }) => {
     ]
 
     return (
-        <div className=' w-[700px] bg-white relative flex m-auto sm:w-auto ml-4 sm:pt-2'>
+        <div className=' w-[700px] bg-white relative flex m-auto sm:w-auto ml-4 sm:pt-4'>
            <div className='text-black absolute top-1 px-2 z-20 flex w-full justify-between'><p>Inspiración:</p> <p>Nuestra Fragancia:</p></div>
             <ImageGaleria
                 items={images} 
@@ -33,8 +33,8 @@ const ImageGallery = ({ img, img2, img3,selectedVolume }) => {
                 
                 />
 <figure className='w-[150px] overflow-hidden '>
-    {selectedVolume==='30ml'?    <img className='object-cover h-[430px] sm:h-full' src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c0f724175559299.64b9a559c7f3a.png" alt="" />
-:    <img className='object-cover h-[430px] sm:h-[300px]' src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/6f8ccd175559299.64c071c0ca990.png" alt="" />
+    {selectedVolume==='30ml'?    <img className='object-cover h-[430px] sm:h-full ' src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c0f724175559299.64b9a559c7f3a.png" alt="" />
+:    <img className='object-cover h-[430px] sm:h-[255px] pr-4 rotate-1' src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/ed7611175559299.66203d3f455d1.png" alt="" />
 
 }
 </figure>
